f2a4b569c1834735ced61518270c846217bb4a6b
Siim Alas <siim.alas@outlook.com>
2021-09-22 10:03:12 +0300

    Initial commit

---
+++ src/vecops.h
@@ -0,0 +1,75 @@
+
+#pragma once
+
+#include <cstddef>
+
+namespace lapp::vecops
+{
+	/*
+	 * Adds a to b component wise for N components.
+	 *
+	 * Parameters:
+	 * 	N, the number of elements to add;
+	 * 	T, the type of elements in a, b, and result;
+	 * 	a, a pointer to the start of one array;
+	 * 	b, a pointer to the start of the other array;
+	 * 	result, a pointer to the start of the array where the result is saved.
+	 *
+	 * Returns:
+	 * 	void
+	 */
+	template <size_t N, typename T>
+	void add(const T* a, const T* b, T* result)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			result[i] = a[i] + b[i];
+		}
+	}
+
+	/*
+	 * Computes the dot product of a and b for N components.
+	 *
+	 * Parameters:
+	 * 	N, the number of elements to multiply and sum;
+	 * 	T, the type of elements in a and b;
+	 * 	a, a pointer to the start of one array;
+	 * 	b, a pointer to the start of the other array;
+	 *
+	 * Returns:
+	 * 	the dot product of the two arrays.
+	 */
+	template <size_t N, typename T>
+	T dot(const T* a, const T* b)
+	{
+		T dot_prod = 0;
+		for (int i = 0; i < N; i++)
+		{
+			dot_prod += a[i] * b[i];
+		}
+		return dot_prod;
+	}
+
+	/*
+	 * Subtracts b from a component wise for N components.
+	 *
+	 * Parameters:
+	 * 	N, the number of elements to subtract;
+	 * 	T, the type of elements in a, b, and result;
+	 * 	a, a pointer to the start of the array from which so subtract b;
+	 * 	b, a pointer to the start of the array to subtract from a;
+	 * 	result, a pointer to the start of the array where the result is saved.
+	 *
+	 * Returns:
+	 * 	void
+	 */
+	template <size_t N, typename T>
+	void subtract(const T* a, const T* b, T* result)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			result[i] = a[i] - b[i];
+		}
+	}
+}
+
---
+++ tests/VecopsSubtract_GivenCorrectInput_SubtractsVectorsCorrectly.cpp
@@ -0,0 +1,17 @@
+
+#include <cstring>
+#include "../src/vecops.h"
+
+int main()
+{
+	const size_t N = 5;
+	double a[N] = { 1, 2, 3, 4, 5 };
+	double b[N] = { 6, 7, 8, 9, 10 };
+	double result[N];
+	double correct_result[N] = { 1 - 6, 2 - 7, 3 - 8, 4 - 9, 5 - 10 };
+
+	lapp::vecops::subtract<N, double>(a, b, result);
+
+	return memcmp(result, correct_result, N);
+}
+
